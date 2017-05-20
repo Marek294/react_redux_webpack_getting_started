9e77a6db94e5dd9f14a8b815e7b18bd6bebab26e
Marek294 <Markra5835@gmail.com>
2017-05-20 19:02:25 +0200

    fix bug

--- client/routes.js
+++ client/routes.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/signup/SignupPage';
 import LoginPage from './components/login/LoginPage';
+import NewEventPage from './components/events/NewEventPage';
 
 
 export default (
@@ -12,5 +13,6 @@ export default (
         <IndexRoute component={Greetings} />
         <Route path="signup" component={SignupPage} />
         <Route path="login" component={LoginPage} />
+        <Route path="new-event" component={NewEventPage} />
     </Route>
 )
